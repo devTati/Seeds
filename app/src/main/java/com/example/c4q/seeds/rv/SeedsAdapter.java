fbe935503e547b8aa256babe1daa73860f17eb2c
Tatiana <tatianamoronta@ac.c4q.nyc>
2018-06-22 08:33:15 -0400

    added uri

--- app/src/main/java/com/example/c4q/seeds/rv/SeedsAdapter.java
+++ app/src/main/java/com/example/c4q/seeds/rv/SeedsAdapter.java
@@ -1,5 +1,8 @@
 package com.example.c4q.seeds.rv;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +19,11 @@ import java.util.List;
 
 public class SeedsAdapter extends RecyclerView.Adapter<SeedsViewHolder> {
     List<Seeds> seedsList;
+    Context context;
+
+    public SeedsAdapter(Context context) {
+        this.context = context;
+    }
 
     public SeedsAdapter(List<Seeds> seedsArrayList) {
         this.seedsList = seedsArrayList;
@@ -24,7 +32,20 @@ public class SeedsAdapter extends RecyclerView.Adapter<SeedsViewHolder> {
     @Override
     public SeedsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false);
+
+        context = view.getContext();
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.foodpantries.org/ci/fl-sarasota");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                context.startActivity(intent);
+            }
+        });
         return new SeedsViewHolder(view);
+
+
     }
 
     @Override
@@ -33,6 +54,9 @@ public class SeedsAdapter extends RecyclerView.Adapter<SeedsViewHolder> {
         Seeds seeds = seedsList.get(position);
 
         holder.onBind(seeds);
+
+
+
 
     }
 
